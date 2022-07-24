f66e468102d1418a96b88850307ea90ba2a814d2
Douglas Mogoba Nyamwaro <mogobanyamwaro@gmail.com>
2022-07-24 11:27:26 +0300

    chore: added navbar

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ const Koa = require("koa");
 const json = require("koa-json");
 const KoaRouter = require("koa-router");
 
+const path = require("path");
+const render = require("koa-ejs");
 const app = new Koa();
 const router = new KoaRouter();
 app.use(json());
 // app.use(async (ctx) => {
 //   ctx.body = { msg: "hello world" };
 // });
+
+render(app, {
+  root: path.join(__dirname, "views"),
+  layout: "layout",
+  viewExt: "html",
+  cache: false,
+  debug: false,
+});
+
+// Index
+router.get("/", async (ctx) => {
+  await ctx.render("index");
+});
 
 router.get("/test", (ctx) => (ctx.body = { msg: "test" }));
 
